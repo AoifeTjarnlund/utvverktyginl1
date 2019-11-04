cfc3848b3cb4e8245c6d93bb710ec003de787d7b
Aoife Tjärnlund <tjarnlunda@gmail.com>
2019-11-04 11:50:37 +0100

    changes to guesser game

--- GuesserGame.java
+++ GuesserGame.java
@@ -1,11 +1,12 @@
+import java.until.Scanner;
 public class GuesserGame {
 
-	public static void(String[] args) {
+	public static void main(String[] args) {
 
 		int low =  Integer.parseInt(args[0]);
 		int high = Integer.parseInt(args[1]);
 		
-		Guesser guesser = new Guesser(low, high)
+		Guesser guesser = new Guesser(low, high);
 		guesser.start();
 
 	}
